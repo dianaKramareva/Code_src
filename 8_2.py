9daf8a4aeb955fa2b8a14036e9b0bd27d5997202
xraweq <xraweq@mail.ru>
2024-07-27 02:53:15 +0300

    8_2

--- 8_2.py
+++ 8_2.py
@@ -1,6 +1,11 @@
-x= int(input("Введите число"))
-while x>=0 :
-    x-=8
-    if x%6==0 and x%10==3 :
-        continue
-    print (x, end=" ")
+x1=float(input())
+x2=float(input())
+step=int(input())
+a=0
+while x1<=x2 :
+    y=1.3*x1*3-8*x1/5
+    print(x1,"->", round(y,3))
+    x1 += step
+    a +=1
+    if a==7:
+        break
